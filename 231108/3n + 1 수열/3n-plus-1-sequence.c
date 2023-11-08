ec4659742ba4c762e6c99a2ca7cd9b83dcb25f4e
yj677 <150113296+yj677@users.noreply.github.com>
2023-11-08 11:32:36 +0900

    Title: 3n + 1 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231108/3n + 1 수열/3n-plus-1-sequence.c
+++ 231108/3n + 1 수열/3n-plus-1-sequence.c
@@ -5,10 +5,11 @@ int main() {
     int n,c=0;
     scanf("%d",&n);
     while(1){
+        if (n ==1) break;
         if (n%2==0) n /= 2;
         else n = 3*n+1;
         c++;
-        if (n ==1) break;
+        
     }
     printf("%d",c);
     return 0;
