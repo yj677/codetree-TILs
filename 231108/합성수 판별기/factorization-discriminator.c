ad0a28751e8558e95ff50ba4a96f7c1bf9961919
yj677 <150113296+yj677@users.noreply.github.com>
2023-11-08 13:27:10 +0900

    Title: 합성수 판별기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231108/합성수 판별기/factorization-discriminator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    bool s = false;
+    scanf("%d",&n);
+    for (int i=1;i<=n;i++){
+        if(n%i==0) s = true;
+    }
+    if (s == true) printf("C");
+    else printf("N");
+    return 0;
+}
