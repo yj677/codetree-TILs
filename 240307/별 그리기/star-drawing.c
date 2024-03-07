f549926115536fe44fcfeb8e381541f970fa14e5
yj677 <150113296+yj677@users.noreply.github.com>
2024-03-07 14:55:36 +0900

    Title: 별 그리기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240307/별 그리기/star-drawing.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d",&n);
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n-i-1; j++){
+            printf(" ");
+        }
+        for (int j=0; j<2*i+1; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (int i=0; i<n-1; i++){
+        for (int j=0; j<i+1; j++){
+            printf(" ");
+        }
+        for (int j=0; j<2*n-2*i-3; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
