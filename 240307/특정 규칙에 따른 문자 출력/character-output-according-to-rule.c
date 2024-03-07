b3f4a23a526190ca330702d40499072e0b648893
yj677 <150113296+yj677@users.noreply.github.com>
2024-03-07 15:00:26 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240307/특정 규칙에 따른 문자 출력/character-output-according-to-rule.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d",&n);
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n-1-i; j++){
+            printf("  ");
+        }
+        for(int j=0; j<i+1; j++){
+            printf("@ ");
+        }
+        printf("\n");
+    }
+
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<n-1-i; j++){
+            printf("@ ");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
