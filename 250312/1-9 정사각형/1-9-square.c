a75c731ede861e37d027cc452a9075c2cafd5a28
yj677 <150113296+yj677@users.noreply.github.com>
2025-03-12 14:49:21 +0900

    Title: 1-9 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250312/1-9 정사각형/1-9-square.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d",&n);
+    int cnt=1;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            printf("%d",cnt);
+            cnt++;
+            if (cnt==10) cnt=1;
+        }
+        printf("\n");
+    }
+    return 0;
+}
