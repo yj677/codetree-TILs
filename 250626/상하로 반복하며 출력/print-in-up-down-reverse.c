070021c29412c489326ca8edfd30a741d674d92f
yj677 <150113296+yj677@users.noreply.github.com>
2025-06-26 16:16:50 +0900

    Title: 상하로 반복하며 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250626/상하로 반복하며 출력/print-in-up-down-reverse.c
+++ 250626/상하로 반복하며 출력/print-in-up-down-reverse.c
@@ -5,8 +5,8 @@ int main() {
     int n;
     scanf("%d",&n);
     for (int i=1; i<=n; i++){
-        for (j=1; j<=n; j++){
-            if (j%2==1){
+        for (int j=1; j<=n; j++){
+            if (j%2==0){
                 printf("%d",n-i+1);
             } else {
                 printf("%d",i);
